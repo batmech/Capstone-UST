2df3a6fd547eebf75b5ea777cba19bef26868b31
batmech <allwynt700@gmail.com>
2024-11-06 10:23:03 +0530

    fixed-password-issue

--- backend/apis/serializers.py
+++ backend/apis/serializers.py
@@ -7,9 +7,27 @@ class BusinessSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class UsersSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = Users
         fields = '__all__'
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = super().create(validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        instance = super().update(instance, validated_data)
+        if password:
+            instance.set_password(password)
+            instance.save()
+        return instance
 
 class EventSerializer(serializers.ModelSerializer):
     business_name = serializers.CharField(source='business.b_name', read_only=True)
